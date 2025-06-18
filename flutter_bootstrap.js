9d2585c550ffd804a0fffd8fe27dc61c90570b47
Rubén Darío González <71092701+gonzalezruben681@users.noreply.github.com>
2025-06-18 11:37:21 -0600

    add: login con qr

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"82bd5b7209295a5b7ff8cae0df96e7870171e3
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1028838661"
+    serviceWorkerVersion: "3006977010"
   }
 });
