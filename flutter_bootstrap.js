788e29529a0707bf4811e837c527cf38867434e7
Rubén Darío González <71092701+gonzalezruben681@users.noreply.github.com>
2025-06-18 11:51:22 -0600

    add: mejoras

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"82bd5b7209295a5b7ff8cae0df96e7870171e3
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3006977010"
+    serviceWorkerVersion: "936334890"
   }
 });
--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -30,7 +30,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "3653f927dcd59e5568ff2495a2984ba1
 "canvaskit/skwasm.worker.js": "89990e8c92bcb123999aa81f7e203b1c",
 "favicon.png": "4bb92599e8f365334e9b0cc70aef2c68",
 "flutter.js": "4b2350e14c6650ba82871f60906437ea",
-"flutter_bootstrap.js": "be979dcc15e21ffab5ee8259025bd009",
+"flutter_bootstrap.js": "fbdcacc02cb354a8cf6bd390c0731b44",
 "icons/Icon-192.png": "616d4f8b6fb7e29271a35d4f68de58f6",
 "icons/Icon-512.png": "b409299574b6b085eb29a6f4122ffd78",
 "icons/Icon-maskable-192.png": "616d4f8b6fb7e29271a35d4f68de58f6",
