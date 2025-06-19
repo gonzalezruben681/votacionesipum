dd261a357cd8c51dd9fc848dc2ccb2ea331c8618
Rubén Darío González <71092701+gonzalezruben681@users.noreply.github.com>
2025-06-18 20:45:23 -0600

    add: pruebas de scanner

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -30,14 +30,14 @@ const RESOURCES = {"assets/AssetManifest.bin": "3653f927dcd59e5568ff2495a2984ba1
 "canvaskit/skwasm.worker.js": "89990e8c92bcb123999aa81f7e203b1c",
 "favicon.png": "4bb92599e8f365334e9b0cc70aef2c68",
 "flutter.js": "4b2350e14c6650ba82871f60906437ea",
-"flutter_bootstrap.js": "287fd76b6a709e33d10ba4d9b8485f0a",
+"flutter_bootstrap.js": "1dddf8bbd38ee78b81f327293a4f2a0b",
 "icons/Icon-192.png": "616d4f8b6fb7e29271a35d4f68de58f6",
 "icons/Icon-512.png": "b409299574b6b085eb29a6f4122ffd78",
 "icons/Icon-maskable-192.png": "616d4f8b6fb7e29271a35d4f68de58f6",
 "icons/Icon-maskable-512.png": "b409299574b6b085eb29a6f4122ffd78",
-"index.html": "736e54b7b600e839521a6828d7c29a97",
-"/": "736e54b7b600e839521a6828d7c29a97",
-"main.dart.js": "3bd4e63f41157cdea5e40502de00eee9",
+"index.html": "b3b823d9e7287d1f679ee92321f9635e",
+"/": "b3b823d9e7287d1f679ee92321f9635e",
+"main.dart.js": "2776dee70a8ea4046e7f9904cd788b1c",
 "manifest.json": "aade780f413c933c2846eccc29476207",
 "version.json": "6d67d71942abb3994228f3bb6365d480"};
 // The application shell files that are downloaded before a service worker can
