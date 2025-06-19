6063ab99774c444777ed2afd17bb181210c2dafb
Rubén Darío González <71092701+gonzalezruben681@users.noreply.github.com>
2025-06-19 09:40:51 -0600

    add: ajustes del scanner

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -30,14 +30,14 @@ const RESOURCES = {"assets/AssetManifest.bin": "3653f927dcd59e5568ff2495a2984ba1
 "canvaskit/skwasm.worker.js": "89990e8c92bcb123999aa81f7e203b1c",
 "favicon.png": "4bb92599e8f365334e9b0cc70aef2c68",
 "flutter.js": "4b2350e14c6650ba82871f60906437ea",
-"flutter_bootstrap.js": "4abcce8db815b76f4b47fe03408d415f",
+"flutter_bootstrap.js": "8fca7a08202142b7be8f4448a58b1c02",
 "icons/Icon-192.png": "616d4f8b6fb7e29271a35d4f68de58f6",
 "icons/Icon-512.png": "b409299574b6b085eb29a6f4122ffd78",
 "icons/Icon-maskable-192.png": "616d4f8b6fb7e29271a35d4f68de58f6",
 "icons/Icon-maskable-512.png": "b409299574b6b085eb29a6f4122ffd78",
 "index.html": "3c3a72e99bf2a45f60c932d9b23dc21c",
 "/": "3c3a72e99bf2a45f60c932d9b23dc21c",
-"main.dart.js": "07aa6279114dba669b120ca5829431c4",
+"main.dart.js": "3a9850aec987c50c8c02d2ce48757ea0",
 "manifest.json": "aade780f413c933c2846eccc29476207",
 "version.json": "6d67d71942abb3994228f3bb6365d480"};
 // The application shell files that are downloaded before a service worker can
