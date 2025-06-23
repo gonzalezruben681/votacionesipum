20bf3ef35f6de9ce6e9295899177310814b7765b
Rubén Darío González <71092701+gonzalezruben681@users.noreply.github.com>
2025-06-23 10:33:23 -0600

    add: mejoras qr y cerrar sesion

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,11 +3,11 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "f0203e27294d658374abd051bd3744d1",
+const RESOURCES = {"flutter_bootstrap.js": "15c088fba35ac5de00daaf9872109594",
 "version.json": "6d67d71942abb3994228f3bb6365d480",
-"index.html": "fd90b2e8862e6d522cd5e5d2ecad5d90",
-"/": "fd90b2e8862e6d522cd5e5d2ecad5d90",
-"main.dart.js": "91994ba3f21c9d618947113b496d4884",
+"index.html": "9705d120566177744a0043f76bd742d0",
+"/": "9705d120566177744a0043f76bd742d0",
+"main.dart.js": "3f488fb52041bf054875d0eaeebf32d1",
 "flutter.js": "4b2350e14c6650ba82871f60906437ea",
 "favicon.png": "4bb92599e8f365334e9b0cc70aef2c68",
 "icons/Icon-192.png": "616d4f8b6fb7e29271a35d4f68de58f6",
